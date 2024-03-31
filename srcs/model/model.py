80c6659b55d74c791beced552c5894a1f8cd51f5
Sergey Shashmurin <shashmurin_s@mail.ru>
2024-03-31 15:13:31 +0600

    feat: add model with pretrained

--- srcs/model/model.py
+++ srcs/model/model.py
@@ -1,5 +1,7 @@
-import torchvision.models.mobilenet
+import torchvision
 
 
 def get_mobilenet_model(num_classes: int = 29):
-    return torchvision.models.mobilenet.mobilenet_v3_small(num_classes=num_classes)
+    model = torchvision.models.mobilenet_v3_small(weights=torchvision.models.mobilenet.MobileNet_V3_Small_Weights)
+    model.classifier[-1].out_features = num_classes
+    return model
